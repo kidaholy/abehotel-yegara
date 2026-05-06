45ec4f1c13b503dee7264795e54cbd7f68aa97de
kidaholy <kidyos2014@gmail.com>
2026-05-06 17:27:38 +0300

    fix(deploy): Passenger CWD fix + force sync static assets + disable cache

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,32 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0, must-revalidate, proxy-revalidate',
+          },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0, must-revalidate, proxy-revalidate',
+          },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ]
+  },
   async rewrites() {
     return [{ source: '/favicon.ico', destination: '/icon.svg' }]
   },
